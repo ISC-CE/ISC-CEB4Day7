a61a099313c5b98893d756af80b8803fb46e40c7
Alex <abraha7@uw.edu>
2023-10-10 19:12:00 -0400

    Complete warm up exercises

--- Exercises/warm-up.js
+++ Exercises/warm-up.js
@@ -1,6 +1,10 @@
 // Create 3 Variables of the following types
-//string
-//number
-//boolean
+let stringVar = "Hello world!"; //string 
+let numberVar = 10; //number
+let booleanVar = true; //boolean
 
 //Print the variables to the console
+
+console.log(stringVar)
+console.log(numberVar)
+console.log(booleanVar)
