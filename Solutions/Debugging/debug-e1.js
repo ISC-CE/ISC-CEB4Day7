8a6991ca082f68fd2ccdf5f58457f329e2e7768f
ofotun <121536704+ofotun@users.noreply.github.com>
2023-10-10 23:05:55 +0100

    Delete Solutions/Debugging directory

--- Solutions/Debugging/debug-e1.js
+++
@@ -1,8 +0,0 @@
-// Incorrect Code in line 3:
-function sayHello() {
-    console.log("Hello, World!");
-}
-
-// Call the function to test it
-//Todo: 
-sayHello();
--- Solutions/Debugging/debug-e3.js
+++
@@ -1,8 +0,0 @@
-/*
-Concept Tutorial:
-In JavaScript, functions are called using parentheses ().
-A missing closing parenthesis can lead to syntax errors.
-*/
-
-// Corrected Code:
-greet("Alice");  // Closing parenthesis added to end the function call
