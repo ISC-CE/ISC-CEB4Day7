04b6b398e8d8f50707635729c0fe8ff6f6a77fe2
rajeev-menon <rajeevmenonwky@gmail.com>
2023-10-10 18:14:29 -0500

    First

--- Exercises/warm-up.js
+++ Exercises/warm-up.js
@@ -1,6 +1,12 @@
 // Create 3 Variables of the following types
-//string
+let stringVar = "hello World!";
+let numberVar = 10;
+let booleanVar = true;
 //number
 //boolean
 
 //Print the variables to the console
+
+console.log(stringVar);
+console.log(numberVar);
+console.log(booleanVar);
