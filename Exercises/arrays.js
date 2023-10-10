e1a5f4209ae31055e3ed4b0cb8c9cbd5de8baac5
rajeev-menon <rajeevmenonwky@gmail.com>
2023-10-10 18:57:49 -0500

    Update 2 Array

--- Exercises/arrays.js
+++ Exercises/arrays.js
@@ -1,12 +1,18 @@
-function largest(arr){
+/*function largest(arr){
     let largest = arr[0];
     for (let i=1; i<arr.length; i++){
         if (arr[i]>largest){
             largest = arr[i];
         }
     }
-return largest;
-
+    return largest;
 }
-lnum = largest([1,2,3,4,5,6]);
-console.log("The largest number of the given array is: ",lnum);
+lnum = largest(1);
+console.log("The largest number of the given array is: ",lnum);*/
+
+
+function getLargest(arg){
+    return Math.max(...arg);
+}
+
+console.log(getLargest([1,2,3,4,5,6]));
