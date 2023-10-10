2f000eade187601f899943c6c5fe6ce0e1bfb05f
ashwin <ashwin@ashwins-MacBook-Pro.local>
2023-10-10 19:28:52 -0400

    Fixes debug-e10.js

--- Debugging/debug-e10.js
+++ Debugging/debug-e10.js
@@ -1,13 +1,14 @@
 // Incorrect Code:
 let person = {
-    name: "Alice",
-    greet: function() {
-      setTimeout(function() {
-        console.log("Hello, my name is " + this.name);
-      }, 1000);
-    }
-  };
-  person.greet();
+  name: "Alice",
+  greet: function() {
+    setTimeout(() => {
+      console.log("Hello, my name is " + this.name);
+    }, 1000);
+  }
+};
+
+person.greet();
   
   // Issue: 'this' context is lost inside the setTimeout function.
   
