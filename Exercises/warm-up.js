90bb083f6f0cb29a8514879cfa0dc207314e7a4d
Shraddha Tighare <shra.tighare@gmail.com>
2023-10-10 19:12:11 -0400

    Doing warmup

--- Exercises/warm-up.js
+++ Exercises/warm-up.js
@@ -1,6 +1,8 @@
-// Create 3 Variables of the following types
-//string
-//number
-//boolean
+let stringVar= "Hello World";
+let numberVar= 10;
+let booleanVar= true;
 
-//Print the variables to the console
+
+console.log(stringVar);
+console.log(numberVar);
+console.log(booleanVar);
