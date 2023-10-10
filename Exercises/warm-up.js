5e666b5eef730ba0d6d014a649e1c627d2971816
Funmilayo A. Fagoyinbo <funmilayofagoyinbo@gmail.com>
2023-10-10 16:10:07 -0700

    executed warm-up exercises

--- Exercises/warm-up.js
+++ Exercises/warm-up.js
@@ -1,6 +1,12 @@
 // Create 3 Variables of the following types
 //string
+let name = "funmi";
 //number
+let number = 10;
 //boolean
+let isfemale = true;
 
 //Print the variables to the console
+console.log(name);
+console.log(number);
+console.log(isfemale);
