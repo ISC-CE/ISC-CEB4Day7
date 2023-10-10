e6892fd2724bbb5a698697e1aceaf6ff8594b8e1
ofotun <121536704+ofotun@users.noreply.github.com>
2023-10-10 23:05:29 +0100

    Delete Solutions/Exercises directory

--- Solutions/Exercises/functions.js
+++
@@ -1,9 +0,0 @@
-// Define a function named 'add' that takes two parameters, a and b
-function add(a, b) {
-    // Return the sum of a and b
-    return a + b;
-  }
-  
-  // Call the function with arguments 5 and 3, and log the result to the console
-  console.log(add(5, 3));  // Output: 8
-  
