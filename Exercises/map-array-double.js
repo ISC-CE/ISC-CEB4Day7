df4adece86dc7253c34d847a6c481204923610bc
sbscproject <fotun@sbsc.com>
2023-10-11 01:18:43 +0100

    Exercise Solutions

---
+++ Exercises/map-array-double.js
@@ -0,0 +1,10 @@
+//Define an array of numbers
+ let initialNumbers = [1,2,3,4,5];
+
+//Use map function to create a new array that duplicates each number in the original array
+let duplicateNumbers = initialNumbers.map(function(number){
+    return number * 2;
+});
+
+console.log(duplicateNumbers);
+//Log the new array to console
